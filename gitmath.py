ce68909bdef04ed3b6e782ccad86e66bb9619b40
JuanSebastianFernandez <juansf_b@hotmail.com>
2025-03-22 17:27:12 -0500

    Se corrige bug de la función division en gitmath.py

--- gitmath.py
+++ gitmath.py
@@ -11,4 +11,6 @@ def exponente(a, b):
     return a**b
 
 def division(a, b):
+    if b == 0:
+        return "Error: Please write a different number to 0 -- Division By zero is not possible"
     return a/b
