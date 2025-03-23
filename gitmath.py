fa49a1fc21edff80cc31cf4d11ba9eaac4aa259a
JuanSebastianFernandez <juansf_b@hotmail.com>
2025-03-22 21:55:38 -0500

    Se agrega funcionalidad raiz cuadrada en gitmath.py

--- gitmath.py
+++ gitmath.py
@@ -1,3 +1,5 @@
+import math
+
 def suma(a, b):
     return a + b
 
@@ -14,3 +16,6 @@ def division(a, b):
     if b == 0:
         return "Error: Please write a different number to 0 -- Division By zero is not possible"
     return a/b
+
+def raiz_2(a):
+    return math.sqrt(a)
